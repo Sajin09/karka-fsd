dcbd6c14abf49085e6c1a666b8f59a7b3290edf5
Sajin09 <sajin0904@gmail.com>
2024-02-19 15:29:55 +0530

    portfolio using jquary

---
+++ JQuery/Portfolio/JQuery/portfolio.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#home-tab, #profile-tab, #contact-tab, #disabled-tab").click(function(){
+        $("#home-tab, #profile-tab, #contact-tab, #disabled-tab").css("background-color", "");
+        $("#home-tab, #profile-tab, #contact-tab, #disabled-tab").css("color", "");
+        $(this).css("background-color", "transparent");
+        $(this).css("color", "white");
+    });
+});
