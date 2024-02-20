d9976035ff3eb7688eb31f5df3c973e16008446d
Sajin09 <sajin0904@gmail.com>
2024-02-20 16:54:41 +0530

    modify portfolio

--- JQuery/Portfolio/JQuery/portfolio.js
+++ JQuery/Portfolio/JQuery/portfolio.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("#home-tab, #profile-tab, #contact-tab, #disabled-tab").click(function(){
         $("#home-tab, #profile-tab, #contact-tab, #disabled-tab").css("background-color", "");
-        $("#home-tab, #profile-tab, #contact-tab, #disabled-tab").css("color", "");
+        $("#home-tab, #profile-tab, #contact-tab, #disabled-tab").css("color", "gold");
         $(this).css("background-color", "transparent");
         $(this).css("color", "white");
     });
